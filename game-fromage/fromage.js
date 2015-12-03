dfc0d04f35b0fdc8f7eefc8401bad0a76d1f897d
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2015-12-04 00:07:15 +0100

    create predador #2

--- game-fromage/fromage.js
+++ game-fromage/fromage.js
@@ -3,13 +3,21 @@ var predador = new Array(2); //head and tail
 var predador_length = 2;
 var dir = "right";
 var food = "";
-var heigh = 
+var heigh = 700;
 var width = 700;
 
 //images to the predador
 predadorHead = new Image();
 predadorHead.src = "resources/head2.png"
+create_predador;
 
 //images to the food
 foodimage = new Image();
 foodimage.src = "resources/cheese.png"
+
+function create_predador(){
+	predador[0] = {xx: 2, yy:: 1};
+	predador[2 = {xx: 1, yy:: 1};
+}
+
+function display(){}
