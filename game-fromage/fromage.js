24b519d9d48522dfe55f03ab2fbf6ea087a25ac9
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2015-12-04 00:00:56 +0100

    Principal variables #2

---
+++ game-fromage/fromage.js
@@ -0,0 +1,15 @@
+var game_over = false;
+var predador = new Array(2); //head and tail
+var predador_length = 2;
+var dir = "right";
+var food = "";
+var heigh = 
+var width = 700;
+
+//images to the predador
+predadorHead = new Image();
+predadorHead.src = "resources/head2.png"
+
+//images to the food
+foodimage = new Image();
+foodimage.src = "resources/cheese.png"
