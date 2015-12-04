def58377ea5a459e9f5d45f3a3d4459ae6789f58
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2015-12-04 01:44:16 +0100

    add file commands to keyboard commands

---
+++ web-page/js/commands.js
@@ -0,0 +1,47 @@
+function playmusic1() {
+    var audio = new Audio('sounds/star.mp4');
+    audio.play();
+}
+function playmusic2() {
+    var audio = new Audio('sounds/cavaco.mp4');
+    audio.play();
+}
+function scrolling(id) {
+    window.location.hash = id;
+}
+function init() {
+	shortcut.add("Ctrl+1", function() {
+	scrolling();
+	});
+	shortcut.add("b", function() {
+		playmusic1();
+	});
+	shortcut.add("c", function() {
+		playmusic2();
+	});
+	shortcut.add("1", function() {
+		scrolling("header");
+	}); 
+	shortcut.add("2", function() {
+		scrolling("services");
+	}); 
+	shortcut.add("3", function() {
+		scrolling("portfolio");
+	}); 
+	shortcut.add("\"", function() {
+		scrolling("portfolio");
+	}); 
+	shortcut.add("4", function() {
+		scrolling("about");
+	}); 
+	shortcut.add("5", function() {
+		scrolling("team");
+	}); 
+	shortcut.add("6", function() {
+		scrolling("clients");
+	}); 
+	shortcut.add("7", function() {
+		scrolling("contact");
+	});
+} 
+window.onload=init;
